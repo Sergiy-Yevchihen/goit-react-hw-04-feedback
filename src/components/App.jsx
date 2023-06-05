d8dc743654013c1e4b5aa135036b284e367eb25d
Serhii Yevchishen <magodza06@gmail.com>
2023-06-05 21:42:22 +0300

    add app huck

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const leaveFeedback = option => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
+    }));
+  };
+
+  const countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = ({ good, neutral, bad }) =>
+    Math.round((good * 100) / countTotalFeedback(feedback));
+
+  const { good, neutral, bad } = feedback;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={leaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback(feedback)}
+          positivePercentage={countPositiveFeedbackPercentage(feedback)}
+        ></Statistics>
+      </Section>
+      <GlobalStyle />
+    </Layout>
   );
 };
+
+export default App;
---
+++ src/components/theme.jsx
@@ -0,0 +1,46 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: 'rgb(65 169 1)',
+    text: '#2a2a2a',
+    background: 'rgb(22 156 163)',
+    primary: '#c6cfff',
+    secondary: 'rgb(52 149 187)',
+    accent: '#656565',
+    muted: 'rgb(81 24 173)',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '2px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '15px',
+    round: '50%',
+  },
+  shadows: {
+    textShadow: '0 1px 1px #fff',
+    boxShadow: 'inset -3px -3px 7px #ffffff',
+  },
+};
